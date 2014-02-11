e763adcbd2a5bb0fee366efa8e4b81dd1522380a
B. van Berkum <dev@dotmpe.com>
2014-02-11 02:01:14 +0100

    config update

--- test-oauth/testconfig.php
+++ test-oauth/testconfig.php
@@ -1,7 +1,7 @@
 <?php
 
 $domain = "dev-berend-sugar7.brixcloud.nl";
-$domain = "everest.cloudcontrol.com/exact-online";
+$domain = "everest.cloudcontrolled.com/test-oauth";
 $config['properties']['oauth_consumer_key'] = '{ab175873-ef27-4655-9834-6f9c90f0d509}';
 $config['properties']['oauth_consumer_secret'] = 'Cdcm2yqvkvFh';
 
