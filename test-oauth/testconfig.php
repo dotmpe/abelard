304ac561abcc198d59ddd14d3fd5892e9997d974
B. van Berkum <berend@brixcrm.nl>
2014-11-02 03:48:34 +0100

    Removed oauth test

--- test-oauth/testconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$domain = "dev-berend-sugar7.brixcloud.nl";
-$domain = "abelard.cloudcontrolled.com/test-oauth";
-$config['properties']['oauth_consumer_key'] = '{ab175873-ef27-4655-9834-6f9c90f0d509}';
-$config['properties']['oauth_consumer_secret'] = 'Cdcm2yqvkvFh';
-
